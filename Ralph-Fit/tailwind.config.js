143deb6ce9e00488312f75e42022b6a875e090c0
Anuj054 <anujc054@gmail.com>
2025-01-17 16:05:20 +0530

    1

--- Ralph-Fit/tailwind.config.js
+++ Ralph-Fit/tailwind.config.js
@@ -1,13 +1,25 @@
 // tailwind.config.js
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // Include all your source files here
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all your source files here
   ],
   theme: {
+    screens: {
+      xs: "380px",
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
-        customBrown: '#6f5c47', // Define the custom color
+        customBrown: "#6f5c47", // Define the custom color
       },
     },
   },
